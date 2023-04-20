76660ef38592a92694d9e8b27eb95099963c3327
ttarabay <tomtarabay@outlook.com>
2023-04-19 21:09:54 -0500

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -293,15 +293,15 @@ const Home = () => {
           const value = parseInt(e.target.value);
           if (value > maxClaimable) {
             setQuantity(maxClaimable);
-          } else if (value < 696969) {
-            setQuantity(696969);
+          } else if (value < 13939393939394) {
+            setQuantity(139393939393949);
           } else {
             setQuantity(value);
           }
         }}
         value={quantity}
         step={696969}
-        defaultValue={696969}
+        defaultValue={13939393939394 }
         className={`${styles.textInput} ${styles.noGapBottom}`}
       />
 
